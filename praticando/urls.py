b66d33bc5951b827ad98d769959935af33475dc0
ThiagoIvens <thiagorech2121@gmail.com>
2021-06-01 01:04:41 -0300

    add a template with form and function to show the page on url /verifica-cpf/ (for implement a cpf validator)

---
+++ praticando/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('verifica-cpf/', views.verificaCpf, name='verfica-cpf' ),
+]
--- praticando/views.py
+++ praticando/views.py
@@ -1,3 +1,6 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def verificaCpf(request):
+    return render(request, 'Geral/verifica_cpf.html')
