d7de1a6461f112c1988843e50133615a27b988d0
ThiagoIvens <thiagorech2121@gmail.com>
2021-06-01 22:26:36 -0300

    Validação de cpf com javascript

--- praticando/views.py
+++ praticando/views.py
@@ -1,6 +1,9 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
+@csrf_exempt
 def verificaCpf(request):
     return render(request, 'Geral/verifica_cpf.html')
--- static/praticando/js/scripts.js
+++ static/praticando/js/scripts.js
@@ -1,1 +1,25 @@
-console.log('DEU')
+
+function TestaCPF() {
+    strCPF = document.getElementById("campo_cpf").value;
+    var Soma;
+    var Resto;
+    Soma = 0;
+    
+    if (strCPF == "") return console.log("Campo cpf vazio");
+
+    if (strCPF == "00000000000") return alert('CPF INVÁLIDO!');
+
+    for (i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(strCPF.substring(9, 10)) ) return alert('CPF INVÁLIDO!');
+
+    Soma = 0;
+    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(strCPF.substring(10, 11) ) ) return alert('CPF INVÁLIDO!');
+    return alert('O CPF É VALIDO!');
+}
